3a4c66c61cda50bf6c92bd7a6d44b968eeac06fe
jgadams97 <me@gmail.com>
2019-09-19 17:57:43 -0400

    Thu Sep 19 17:57:43 EDT 2019

--- app/src/main/java/com/codered/wakeupwithintent/BluetoothActivity.java
+++ app/src/main/java/com/codered/wakeupwithintent/BluetoothActivity.java
@@ -5,13 +5,22 @@ import android.app.*;
 import android.os.*;
 import android.view.*;
 import android.content.BroadcastReceiver;
+import java.awt.event.KeyEvent;
 
 public class BluetoothActivity extends BroadcastReceiver
 {
+	public static int received = 0;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        
-    }
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		
+		KeyEvent event = (KeyEvent)intent.getParcelableExtra(Intent.EXTRA_KEY_EVENT);
+		BluetoothActivity.received = event.getKeyCode();
+		//if (KeyEvent.KEYCODE_MEDIA_PLAY == event.getKeyCode()) {    
+                //call my method    
+		//}    
+		//if (Intent.ACTION_MEDIA_BUTTON.equals(intent.getAction())) {    
+		 
+		//}    
+	}    
 }
