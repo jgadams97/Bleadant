5cc59dd8c1ca78c635d42166291afc889cf62e14
jgadams97 <me@gmail.com>
2019-09-19 20:39:42 -0400

    Thu Sep 19 20:39:42 EDT 2019

--- app/src/main/java/com/codered/wakeupwithintent/MainActivity.java
+++ app/src/main/java/com/codered/wakeupwithintent/MainActivity.java
@@ -61,8 +61,8 @@ public class MainActivity extends Activity
 	}
 	private boolean checkForPermission(Context context) {
 		AppOpsManager appOps = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
-		int mode = appOps.checkOpNoThrow(OPSTR_GET_USAGE_STATS, Process.myUid(), context.getPackageName());
-		return mode == MODE_ALLOWED;
+		int mode = appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, Process.myUid(), context.getPackageName());
+		return mode == AppOpsManager.MODE_ALLOWED;
 	}
 
     public static String getForegroundProcess(Context context) {
