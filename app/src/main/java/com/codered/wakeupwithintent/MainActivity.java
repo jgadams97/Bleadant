fe4932caae9564241ed343e46f8786126705f818
jgadams97 <me@gmail.com>
2019-09-19 20:31:34 -0400

    Thu Sep 19 20:31:34 EDT 2019

--- app/src/main/java/com/codered/wakeupwithintent/MainActivity.java
+++ app/src/main/java/com/codered/wakeupwithintent/MainActivity.java
@@ -72,6 +72,30 @@ public class MainActivity extends Activity
 		}
 	}
 
+    public static String getForegroundProcess(Context context) {
+        String topPackageName = null;
+        UsageStatsManager usage = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
+        long time = System.currentTimeMillis();
+        List<UsageStats> stats = usage.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, time - 1000*1000, time);
+        if (stats != null) {
+            SortedMap<Long, UsageStats> runningTask = new TreeMap<Long,UsageStats>();
+            for (UsageStats usageStats : stats) {
+                runningTask.put(usageStats.getLastTimeUsed(), usageStats);
+            }
+            if (runningTask.isEmpty()) {
+                return null;
+            }
+            topPackageName =  runningTask.get(runningTask.lastKey()).getPackageName();
+        }
+        if(topPackageName==null) {
+            Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
+            context.startActivity(intent);
+        }
+
+        return topPackageName;
+    }
+
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
